3d7679d8f3633dee570df5b3ac99546521c9467b
manharmangat07-gif <manharmangat07@gmail.com>
2025-10-01 00:24:18 +0530

    Create prog34.c

---
+++ Day 17/prog34.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, flag = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n <= 1) {
+        printf("%d is Not Prime", n);
+    } else {
+        for(i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0) {
+            printf("%d is Prime", n);
+        } else {
+            printf("%d is Not Prime", n);
